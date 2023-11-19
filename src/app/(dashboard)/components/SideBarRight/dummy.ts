3d3dce79a1a52fec0d9342bf254d05a7e95839bd
huynhhuuy1999 <huynhhuuy1@gmail.com>
2023-11-19 15:35:42 +0700

    add folder sidebarRIght

---
+++ src/app/(dashboard)/components/SideBarRight/dummy.ts
@@ -0,0 +1,29 @@
+import { ICardUserMess } from "@/models/components";
+
+export const listUserMess: ICardUserMess[] = [
+  {
+    avatar: "",
+    name: "Phạm Minh Thư",
+    haveStory: true,
+  },
+  {
+    avatar: "",
+    name: "Hữu Trường",
+    haveStory: false,
+  },
+  {
+    avatar: "",
+    name: "Hồng nga",
+    haveStory: false,
+  },
+  {
+    avatar: "",
+    name: "Sang Lê",
+    haveStory: false,
+  },
+  {
+    avatar: "",
+    name: "Nguyễn Phúc",
+    haveStory: false,
+  },
+];
